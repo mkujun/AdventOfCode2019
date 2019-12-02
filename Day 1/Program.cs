94962dd18f0badb0fadcef4f9e51c968207c103e
mkujun <kujundzic.marko@hotmail.com>
2019-12-02 21:34:49 +0100

    day 2 part 1

---
+++ Day 1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_1
+{
+    class Program
+    {
+        // part 1
+        static int TankMass(int input)
+        {
+            int mass = 0;
+
+            return mass += (input / 3) - 2;
+        }
+
+        // part 2
+        static int FuelMass(int input, int fuelMassByTank)
+        {
+            int mass = 0;
+
+            mass += (input / 3) - 2;
+
+            if(mass <= 0)
+            {
+                return fuelMassByTank;
+            }
+
+            return FuelMass(mass, fuelMassByTank + mass);
+        }
+
+        static string[] ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] input = File.ReadAllLines(path);
+
+                return input;
+            }
+
+            return null;
+        }
+
+        static List<int> ConvertStringArrayToIntList(string[] stringArray)
+        {
+            List<int> intList = new List<int>();
+
+            if (stringArray != null)
+            {
+                foreach (var item in stringArray)
+                {
+                    intList.Add(Int32.Parse(item));
+                }
+            }
+
+            return intList;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] stringInput = ReadFile("your path to file...");
+            List<int> intInput = ConvertStringArrayToIntList(stringInput);
+
+            int fuelMass = 0;
+
+            foreach (var tank in intInput)
+            {
+                fuelMass += (FuelMass(tank, 0));
+            }
+
+            int tankMass = 0;
+
+            foreach (var tank in intInput)
+            {
+                tankMass += TankMass(tank);
+            }
+
+            Console.WriteLine("Hello Day 1!");
+        }
+    }
+}
--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Day_2
 {
     class Program
     {
+        static int[] ReadFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] input = File.ReadAllText(path).Split(',');
+
+                int[] myInt = Array.ConvertAll(input, s => int.Parse(s));
+
+                return myInt;
+            }
+
+            return null;
+        }
+
+        static int[] CalculateOpcode(int[] data)
+        {
+            for (int i = 0; i < data.Length; i = i + 4)
+            {
+                int firstPosition = data[i + 1];
+                int secondPosition = data[i + 2];
+                int destination = data[i + 3];
+
+                if (data[i] == 1)
+                {
+                    int add = data[firstPosition] + data[secondPosition];
+
+                    data[destination] = add;
+                }
+                if (data[i] == 2)
+                {
+                    int multiply = data[firstPosition] * data[secondPosition];
+
+                    data[destination] = multiply;
+                }
+                if (data[i] == 99)
+                {
+                    return data;
+                }
+            }
+
+            return data;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] input = ReadFile("path to file");
+
+            int[] inputData = input;
+            inputData[1] = 12;
+            inputData[2] = 2;
+
+            CalculateOpcode(inputData);
+
+            Console.WriteLine("Hello Day 2 part 1!");
         }
     }
 }
