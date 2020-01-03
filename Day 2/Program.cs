2508389c5337426918a7ba37cea4d020e7f600c6
mkujun <kujundzic.marko@hotmail.com>
2020-01-03 20:37:05 +0100

    day 2

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -6,6 +6,8 @@ namespace Day_2
 {
     class Program
     {
+        static string FilePath = "path to file";
+
         static int[] ReadFile(string path)
         {
             if (File.Exists(path))
@@ -49,17 +51,43 @@ namespace Day_2
             return data;
         }
 
+        static Tuple<int,int> FindNounAndVerb(int output)
+        {
+            int[] opcodeResult;
+
+            for (int i = 0; i <= 99; i++)
+            {
+                for (int j = 0; j <= 99; j++)
+                {
+                    // call function each time with fresh input
+                    int[] data = ReadFile(FilePath);
+                    data[1] = i;
+                    data[2] = j;
+                    opcodeResult = CalculateOpcode(data);
+
+                    if (opcodeResult[0] == output)
+                    {
+                        return new Tuple<int, int>(opcodeResult[1], opcodeResult[2]);
+                    }
+                }
+            }
+
+            return null;
+        }
+
         static void Main(string[] args)
         {
-            int[] input = ReadFile("path to file");
+            // part 1
+            int[] input1 = ReadFile(FilePath);
+            input1[1] = 12;
+            input1[2] = 2;
 
-            int[] inputData = input;
-            inputData[1] = 12;
-            inputData[2] = 2;
+            CalculateOpcode(input1);
 
-            CalculateOpcode(inputData);
+            // part 2
+            Tuple<int, int> VerbNoun = FindNounAndVerb(19690720);
 
-            Console.WriteLine("Hello Day 2 part 1!");
+            Console.WriteLine("Hello Day 2");
         }
     }
 }
