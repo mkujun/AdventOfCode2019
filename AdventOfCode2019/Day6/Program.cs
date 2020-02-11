6761b801346f23d8f34461c8e6b438e8e550e4a2
mkujun <markok@aa.com>
2020-02-11 15:24:19 +0100

    basic tree structure blueprint for day6

---
+++ AdventOfCode2019/Day6/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Day6
+{
+    class Program
+    {
+        public class TreeNode<T>
+        {
+            public T Data { get; set; }
+            public TreeNode<T> Parent { get; set; }
+            public List<TreeNode<T>> Children { get; set; }
+
+            public TreeNode(T data)
+            {
+                Data = data;
+                Children = new List<TreeNode<T>>();
+            }
+
+            // find parent, then add child
+            public TreeNode<T> AddChild(T child)
+            {
+                TreeNode<T> childNode = new TreeNode<T>(child) { Parent = this };
+                Children.Add(childNode);
+                return childNode;
+            }
+
+            // todo: finish this!!
+            public TreeNode<T> FindParent(T parent)
+            {
+                return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode<int> root = new TreeNode<int>(0);
+            {
+                TreeNode<int> child1 = root.AddChild(1);
+                TreeNode<int> child2 = root.AddChild(2);
+                {
+                    TreeNode<int> child21 = child2.AddChild(21);
+                    TreeNode<int> child22 = child2.AddChild(22);
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
