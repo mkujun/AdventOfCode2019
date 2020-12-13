72759dd4081639794c408cf920595f2375f9e69f
mkujun <kujundzic.marko@hotmail.com>
2020-12-13 18:02:55 +0100

    shitty but working solution to day 6 part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -38,15 +38,12 @@ namespace Day6
             {
                 bool isPlanetInList = false;
 
-                // mapa je prazna
                 if (PlanetNodes.Count == 0)
                 {
                     PlanetNode planetNode = new PlanetNode(planet, orbit);
                     PlanetNodes.Add(planetNode);
                 }
-                // prodji kroz listu
-                // gledaj svaki element ima li medju orbitama planet
-                // ako ima, dodaj orbitu
+
                 else
                 {
                     for (int i = 0; i < PlanetNodes.Count; i++)
@@ -64,7 +61,6 @@ namespace Day6
                         }
                     }
 
-                    // ako planet nije u listi dodaj ga
                     if(!isPlanetInList)
                     {
                         PlanetNode planetNode = new PlanetNode(planet, orbit);
@@ -75,7 +71,28 @@ namespace Day6
 
             }
 
-            internal void FindCOM()
+        }
+        
+        public class Vertex
+        {
+            public string Name;
+            public List<string> Edges;
+
+            public Vertex(string name)
+            {
+                Name = name;
+                Edges = new List<string>();
+            }
+
+            public void AddEdge(string edge)
+            {
+                Edges.Add(edge);
+            }
+        }
+
+        public class Graph
+        {
+            internal void FindCOM(string[] inputData)
             {
                 for (int i = 0; i < inputData.Length; i++)
                 {
@@ -84,14 +101,17 @@ namespace Day6
 
                     if (planet == "COM")
                     {
-                        AddToMap(planet, orbit);
-                        FindNext(orbit);
+                        //AddToMap(planet, orbit);
+                        AddVertex(planet, orbit);
+                        FindNext(orbit, inputData);
                     }
                 }
             }
 
-            internal void FindNext(string planetToFind)
+            internal void FindNext(string planetToFind, string[] inputData)
             {
+                Console.WriteLine("Planet" + planetToFind);
+
                 for (int i = 0; i < inputData.Length; i++)
                 {
                     string planet = inputData[i].Split(")")[0];
@@ -99,26 +119,219 @@ namespace Day6
 
                     if(planet == planetToFind)
                     {
-                        AddToMap(planet, orbit);
-                        FindNext(orbit);
+                        AddVertex(planet, orbit);
+                        FindNext(orbit, inputData);
                     } 
                 }
             }
+            
+            public List<Vertex> Vertices;
+
+            public List<Vertex> YouParents;
+            public List<Vertex> SanParents;
+            public List<Vertex> Ancestors;
+            public List<string> DfsVisited;
+
+            public int youCounter { get; private set; }
+            public int sanCounter { get; private set; }
+
+            public bool sanFound { get; private set; }
+            public bool youFound { get; private set; }
+
+            public Graph()
+            {
+                Vertices = new List<Vertex>();
+                YouParents = new List<Vertex>();
+                SanParents = new List<Vertex>();
+                Ancestors = new List<Vertex>();
+                DfsVisited = new List<string>();
+            }
+
+            public void FindYouParents(string child)
+            {
+                if (child == "COM")
+                {
+                    return;
+                }
+                else
+                {
+                    foreach (Vertex item in Vertices)
+                    {
+                        if (item.Edges.Contains(child))
+                        {
+                            YouParents.Add(item);
+                            FindYouParents(item.Name);
+                        }
+                    }
+
+                }
+            }
+            public void FindSanParents(string child)
+            {
+                if (child == "COM")
+                {
+                    return;
+                }
+                else
+                {
+                    foreach (Vertex item in Vertices)
+                    {
+                        if (item.Edges.Contains(child))
+                        {
+                            SanParents.Add(item);
+                            FindSanParents(item.Name);
+                        }
+                    }
+
+                }
+            }
+
+            public void FindAncestors()
+            {
+                var names = YouParents.Select(x => x.Name).Intersect(SanParents.Select(x => x.Name));
+                foreach (var item in names)
+                {
+                    Ancestors.Add(new Vertex(item));
+                }
+
+                foreach (var item in Ancestors)
+                {
+                    Vertex pero = Vertices.Find(a => a.Name == item.Name);
+
+                    BFS(pero);
+                    int ivo = youCounter + sanCounter;
+                    Console.WriteLine("rez:" + ivo);
+                }
+            }
+
+            public void BFS(Vertex v)
+            {
+                Vertex YOU = new Vertex("YOU");
+                Vertex SAN = new Vertex("SAN");
+
+                List<Vertex> vertices = new List<Vertex>();
+                List<Vertex> temp = new List<Vertex>();
+
+                foreach (var item in v.Edges)
+                {
+                    Vertex vertex = Vertices.Find(v => v.Name == item);
+                    vertices.Add(vertex);
+                }
+
+                do
+                {
+                    if (youFound == false)
+                    {
+                        if (vertices.Find(v => v.Name == "YOU") == null)
+                        {
+                            youCounter++;
+                        }
+                        else
+                        {
+                            youFound = true;
+                        }
+                    }
+                    if (sanFound == false)
+                    {
+                        if (vertices.Find(v => v.Name == "SAN") == null)
+                        {
+                            sanCounter++;
+                        }
+                        else
+                        {
+                            sanFound = true;
+                        }
+                    }
+                    
+
+                    foreach (var item in vertices)
+                    {
+                        foreach (var edge in item.Edges)
+                        {
+                            Vertex vertex = Vertices.Find(v => v.Name == edge);
+
+                            temp.Add(vertex);
+                        }
+                    }
+
+                    vertices.Clear();
+                    foreach (var item in temp)
+                    {
+                        vertices.Add(item);
+                    }
+                    temp.Clear();
+
+                } while (!sanFound || !youFound);
+
+            }
+
+            public void AddVertex(string planet, string orbit)
+            {
+                if (Vertices.Count == 0)
+                {
+                    Vertex vertex = new Vertex(planet);
+                    vertex.Edges.Add(orbit);
+                    Vertices.Add(vertex);
+
+                    Vertex vertex1 = new Vertex(orbit);
+                    Vertices.Add(vertex1);
+                }
+                else
+                {
+                    Vertex vertex = Vertices.Find(v => v.Name == planet);
+
+                    if (vertex != null)
+                    {
+                        vertex.AddEdge(orbit);
+                    }
+
+                    else
+                    {
+                        Vertex vertex1 = new Vertex(planet);
+                        vertex1.AddEdge(orbit);
+                        Vertices.Add(vertex1);
+                    }
+
+                    Vertex vertexOrbit = Vertices.Find(v => v.Name == orbit);
+                    if (vertexOrbit == null)
+                    {
+                        Vertex vertex1 = new Vertex(orbit);
+                        Vertices.Add(vertex1);
+                    }
+
+                }
+            }
         }
-        
+
+
         static void Main(string[] args)
         {
             //string[] input = File.ReadAllLines(@"C:\Users\Korisnik\AdventOfCode2019\Day6\input.txt");
             string[] input = File.ReadAllLines(@"C:\Users\Korisnik\AdventOfCode2019\Day6\taskInput.txt");
-            Map mapa = new Map(input);
-            mapa.FindCOM();
+            //Map mapa = new Map(input);
+            //mapa.FindCOM();
 
+            /*
             int count = 0;
             foreach (var item in mapa.PlanetNodes)
             {
                 count = count + item.Orbits.Count;
             }
+            */
 
+            Graph graph = new Graph();
+            for (int i = 0; i < input.Length; i++)
+            {
+                string planet = input[i].Split(")")[0];
+                string orbit = input[i].Split(")")[1];
+
+                graph.AddVertex(planet, orbit);
+            }
+            //graph.FindCOM(input);
+
+            graph.FindYouParents("YOU");
+            graph.FindSanParents("SAN");
+            graph.FindAncestors();
             Console.WriteLine("test");
         }
     }
